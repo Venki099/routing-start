897961fc334a2fa30b29f13f8351fccc345a7726
Venki099 <venkatesh.s@datalyzer.com>
2023-10-06 07:33:26 +0530

    Routes queryparam routing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,14 +11,11 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
-import { Routes,RouterModule } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AppRoutingModule } from './app-routing.module';
 // import {HttpModule} from'@angular/http'
 
-const appRoutes:Routes = [
-  {path:'',component:HomeComponent},
-  {path:'users',component:UsersComponent},
-  {path:'servers',component:ServersComponent}
-]
+
 
 @NgModule({
   declarations: [
@@ -28,12 +25,13 @@ const appRoutes:Routes = [
     ServersComponent,
     UserComponent,
     EditServerComponent,
-    ServerComponent
+    ServerComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot(appRoutes)
+    AppRoutingModule
   ],
   providers: [ServersService],
   bootstrap: [AppComponent]
